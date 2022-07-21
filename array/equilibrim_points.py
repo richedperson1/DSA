ee0cf7a6c43952dc3539761111e949d9326909b3
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-07-21 13:58:05 +0530

    Adding 3 different way of solution in equilibrim_points files

--- array/equilibrim_points.py
+++ array/equilibrim_points.py
@@ -1,18 +1,59 @@
-arr = [1, 1]
+# arr = [1, 1]
 
-total = sum(arr)
+
+def naive_sol(arr, n):
+    # n = len(arr)
+    if n <= 1:
+        return(1)
+    if n == 2:
+        return -1
+
+    for i in range(1, n):
+        local1 = 0
+        for j in range(i-1, -1, -1):
+            local1 += arr[j]
+        local2 = 0
+        for j in range(i+1, n):
+            local2 += arr[j]
+        if local1 == local2:
+            return i+1
+    return -1
+
+
+def better_solution(arr, n):
+
+    ans = [0]*n
+    total = 0
+    for i in range(n-1, -1, -1):
+        total += arr[i]
+        ans[i] += total
+
+    local1 = arr[0]
+    for j in range(1, n):
+        check = ans[j]-arr[j]
+        if check == local1:
+            return j+1
+        local1 += arr[j]
+
+    return -1
+
+
+def optimize_solution(arr, n):
+    total = sum(arr)
+    n = len(arr)
+    if n <= 1:
+        return(1)
+    prefix = arr[0]
+    for i in range(1, n):
+        local = total-prefix-arr[i]
+        if local == prefix:
+            return(i+1)
+        prefix += arr[i]
+    return(-1)
+
+
+arr = [0, 100, 6, 100, 0]
 n = len(arr)
-if n <= 1:
-    print(1)
-    quit()
-prefix = arr[0]
-for i in range(1, n):
-    local = total-prefix-arr[i]
-    if local == prefix:
-        print(i+1)
-        quit()
-    prefix += arr[i]
-print(-1)
-
-# 5
-# 1 3 5 2 2
+print(naive_sol(arr, n))
+print(better_solution(arr, n))
+print(optimize_solution(arr, n))
