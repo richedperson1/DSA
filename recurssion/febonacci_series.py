f636d3ab76da333ba3ab5ae4e47dfbd2d3af80c9
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-07-29 11:27:50 +0530

    Praticing febonacci series again for revising. while revising some points I got >> 1. I can imagine whole tree how it's working, 2.easily getting thing which is hard for me when I was start

--- recurssion/febonacci_series.py
+++ recurssion/febonacci_series.py
@@ -1,10 +1,18 @@
-num = 4
+
+# def febo(num):
+#     if num <= 1:
+#         return num
+#     return febo(num-2)+febo(num-1)
 
 
-def febo(num):
-    if num <= 1:
-        return num
-    return febo(num-2)+febo(num-1)
+# print(febo(num))
 
 
-print(febo(num))
+def febo_update(n):
+    if n <= 1:
+        print(n, end="-->")
+        return 1
+    return febo_update(n-1)+febo_update(n-2)
+
+
+print(febo_update(5))
