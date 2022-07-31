e760d26d75c035e22958cc4c6373be2713bbe013
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-07-31 14:47:23 +0530

    Modifing [sum_of_n_number.py] file in recurssion adding sum of array function in it

--- recurssion/sum_of_n_number.py
+++ recurssion/sum_of_n_number.py
@@ -23,5 +23,22 @@ def sum_n(n):
     return int((n*(n+1))/2)
 
 
-print(sum_of_n(n))
-print(sum_n(n))
+# print(sum_of_n(n))
+# print(sum_n(n))
+
+"""
+Sum of the array using recursion 
+"""
+
+
+def sum_of_array(arr, sumi, i):
+    if i < 0:
+        return sumi
+    sumi += arr[i]
+    return sum_of_array(arr, sumi, i-1)
+
+
+arr = [5, 5]
+
+print(sum_of_array(arr, 0, len(arr)-1))
+print(sum(arr))
