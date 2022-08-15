35374d3fe521d8ef1450f0b99dd8e94caf3957b7
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-08-15 11:33:03 +0530

    Adding [Reverse a linked list.py] file into the linked list directory

---
+++ Linked list/Reverse a linked list.py
@@ -0,0 +1,51 @@
+"""
+Given a linked list of N nodes. The task is to reverse this list.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5->6
+Output: 6 5 4 3 2 1
+Explanation: After reversing the list, 
+elements are 6->5->4->3->2->1.
+
+[2, 4, 5, 6, 8, 9])
+"""
+from linked_list import linked
+
+
+class node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def __repr__(self) -> str:
+        return self.data
+
+
+def reverse_ll(head):
+    perv = None
+    cur = head
+    nxt = head.next
+
+    while cur != None:
+        nxt = cur.next
+        cur.next = perv
+        perv = cur
+        cur = nxt
+    head = perv
+
+    while head.next:
+        print(head.data, end=" -----> ")
+        head = head.next
+    print(head.data)
+
+
+if __name__ == "__main__":
+    head = None
+    link_obj = linked(head)
+    link_obj.create_ll()
+    link_obj.printing_linked_list()
+    head = link_obj.head
+    reverse_ll(head)
+    # linked.printing_linked_list_external(head)
