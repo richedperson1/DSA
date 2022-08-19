3da61e11694ada64816d15ac6a19917e3f2fe5aa
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-08-19 20:30:37 +0530

    Modifing [detect and remove loop in linked list.py] file from linked list directory added to operete in the O(n) and space is O(1)

--- Linked list/detect and remove loop in linked list.py
+++ Linked list/detect and remove loop in linked list.py
@@ -1,24 +1,50 @@
+from linked_list import *
+
+
 class Node:
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
 
+def detect_circle(head):
+    fast = head.next.next
+    slow = head.next
+    while fast and fast.next != None:
+        if slow == fast:
+            return fast
+        fast = fast.next.next
+        slow = slow.next
+    return False
+
+
 def removeLoop(head: Node) -> Node:
 
     # Write your code
+    modifiend = head
     curr = head
     if head == None or head.next == None:
         return head
-    dist = {}
-    dist[head] = head.val
-    if True:
-        while head:
-            if head.next == None:
-                return curr
-            if head.next in dist:
-                head.next = None
-                return curr
-            head = head.next
-            dist[head] = head.val
-    return curr
+    fast = detect_circle(curr)
+    if fast == False:
+        return modifiend
+    slow = head
+    while fast.next != slow.next:
+        fast = fast.next
+        slow = slow.next
+    if fast.next == slow.next:
+        fast.next = None
+    return head
+
+
+if __name__ == "__main__":
+    head = None
+    obj = linked(head)
+    arr = list(map(int, "893 166 16 729 551 296 361 478 304 301 920 676 675 828 324 888 959 409 548 785 377 317 824 121 596 677 779 863 929 433 206 555 906 705 184 749 47 417 415 683".split()))
+    k = 2
+    obj.create_ll(arr)
+    head = obj.head.next
+
+    is_true = removeLoop(head)
+    linked.printing_linked_list_external(is_true)
+    print("Time Loop is ended  ------> 🤗")
