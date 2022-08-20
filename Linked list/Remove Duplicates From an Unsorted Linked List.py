384c612bec98717e54d535366994fab8e100b7ae
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-08-20 16:22:03 +0530

    Modifing [Remove Duplicate From an Unsorted Linked List.py] file adding better and optimal approach

--- Linked list/Remove Duplicates From an Unsorted Linked List.py
+++ Linked list/Remove Duplicates From an Unsorted Linked List.py
@@ -1,5 +1,48 @@
 from linked_list import *
 
+"""
+Remove duplicate from the linked list 
+Time Complexity : O(n^2)
+Space Complexity : O(1)
+"""
+
+
+def remove_duplicate(head):
+    curr = head
+    while curr.next:
+        local = curr
+        while local and local.next:
+            if local.next.data == curr.data:
+                temp = local.next
+                local.next = temp.next
+            else:
+                local = local.next
+        curr = curr.next
+    return head
+
+
+"""
+More rigid code 
+Time and space complexity is same as above
+
+"""
+
+
+def remove_duplicate_2(head):
+    curr = head
+    while curr.next:
+        local = curr
+        while local and local.next:
+            if local.next.data == curr.data:
+                local_3 = local.next
+                while local_3 and local_3.data == curr.data:
+                    local_3 = local_3.next
+                local.next = local_3
+            else:
+                local = local.next
+        curr = curr.next
+    return head
+
 
 def removeDuplicates(head):
     curr = head
@@ -16,11 +59,17 @@ def removeDuplicates(head):
 
 
 if __name__ == "__main__":
-    lst = list(map(int, ("3 2 3 4 2 3").split()))
-    # print(lst)
+    lst = list(map(int, ("3 3 3 3 1 3 3 ").split()))
     head = None
     obj = linked(head)
     obj.create_ll(lst)
     head = obj.head.next
-    obj_head = removeDuplicates(head)
-    linked.printing_linked_list_external(obj_head)
+    head2 = head
+    obj_head_1 = remove_duplicate_2(head)
+    linked.printing_linked_list_external(obj_head_1)
+
+    # obj_head_2 = removeDuplicates(head)
+    # linked.printing_linked_list_external(obj_head_2)
+
+    # obj_head_3 = remove_duplicate(head)
+    # linked.printing_linked_list_external(obj_head_3)
