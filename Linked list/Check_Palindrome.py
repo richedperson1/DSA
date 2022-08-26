6e0fdb18c25f05eb8ebfa1ac3af8ed333366d657
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-08-26 09:43:50 +0530

    Adding [Check_Palindrome.py] file into the linked list

--- Linked list/Check_Palindrome.py
+++ Linked list/Check_Palindrome.py
@@ -11,9 +11,36 @@ def check_palimdrom(head):
     print(form == form[::-1])
 
 
+def checking_palimdrom(head):
+    if head.next == None:
+        return True
+    fast = head
+    slow = head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+    prev = None
+    while slow:
+        temp = slow.next
+        slow.next = prev
+        prev = slow
+        slow = temp
+
+    while head and prev:
+        if head.data != prev.data:
+            return False
+
+        head = head.next
+        prev = prev.next
+    return True
+
+
 if __name__ == "__main__":
     linked_list = linked(None)
-    arr = [2, 1, 2]
+    arr = [1, 2]
     linked_list.create_ll(arr)
     head = linked_list.head.next
-    check_palimdrom(head)
+
+    head = checking_palimdrom(head)
+    print(head)
