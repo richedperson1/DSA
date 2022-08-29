0dd933611e889e2c03498ffc949588136b9c125a
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-08-29 12:57:55 +0530

    Adding binary tree input.py file into the tree directory

--- binary tree/binary tree input.py
+++ binary tree/binary tree input.py
@@ -6,6 +6,7 @@ def user_tree_create(root, arr, i):
         return root
     if root.left and root.right:
         user_tree_create(root.left, arr, i)
+        # user_tree_create(root.right, arr, i+2)
     elif root.right:
         root.left = root_node(arr[i])
         user_tree_create(root, arr, i+1)
@@ -14,7 +15,22 @@ def user_tree_create(root, arr, i):
         user_tree_create(root, arr, i+1)
 
 
+def createTree() -> root_node:
+    rootData = int(input("Enter the value : "))
+    if rootData <= -1:
+        return None
+    root = root_node(rootData)
+    leftNode = createTree()
+    rightNode = createTree()
+    root.left = leftNode
+    root.right = rightNode
+    return root
+
+
 root = root_node(0)
-lsr = [2, 3, 4, 5]
+lsr = [2, 3, 4, 5, 9, 6]
 rooting = user_tree_create(root, lsr, 0)
 root_node.print_tree(root)
+
+# head_node = createTree()
+# root_node.print_tree(head_node)
