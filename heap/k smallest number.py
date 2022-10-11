d64e082324e6f22dd8e16e4439d64c5532a7e3db
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-10-11 21:11:16 +0530

    is tree heap problem is solve

--- heap/k smallest number.py
+++ heap/k smallest number.py
@@ -1,17 +1,49 @@
-import heapq
+# import heapq
+from heapClass import *
 # heap = heapq
-k = 4
-items = [2, 5, 7, 80, 30, 12, 59, 63]
-heap = []
-for item in items:
-    if len(heap) < k:
-        heapq.heappush(heap, -item)
-    else:
-        if -1*heap[0] > item:
-            # heap = heap[1:]
-            heapq.heappop(heap)
-            heapq.heappush(heap, -item)
-            continue
 
-heapq.heapify(items)
-print(heapq.nsmallest(5, items))
+##
+""" 
+Method 1 using heapq class
+
+"""
+# k = 4
+# items = [2, 5, 7, 80, 30, 12, 59, 63]
+# heap = []
+# for item in items:
+#     if len(heap) < k:
+#         heapq.heappush(heap, -item)
+#     else:
+#         if -1*heap[0] > item:
+#             # heap = heap[1:]
+#             heapq.heappop(heap)
+#             heapq.heappush(heap, -item)
+#             continue
+
+# heapq.heapify(items)
+# print(heapq.nsmallest(5, items))
+
+
+""" 
+Method 2 using custom class
+
+"""
+
+
+class ksmallest(heap):
+    def ksmall(self, arr, k):
+        for num in arr:
+
+            if self.size < k:
+                self.insert(num)
+            elif self.size >= k and self.storage[1] > num:
+                self.delete()
+                self.insert(num)
+        return self.storage[1:]
+
+
+arr = [2, 13, 4, 10, 19, 16, 12, 36]
+k = 3
+print(sorted(arr)[:k][::-1])
+ans = ksmallest().ksmall(arr, k)
+print(ans)
--- heap/tree is heap or not.py
+++ heap/tree is heap or not.py
@@ -1,32 +1,37 @@
-from binary_tree import *
 
 from binary_tree import *
-arr = [8, 7, 6, -1, -1, 5, 4]
+arr = [1, 2, -1, 3]
 tree = createTree()
 nodes = tree.createTreeUsingList(arr)
 
 
-def isHeap(root):
-    if root == None or not(root.left) or not(root.right):
-        return True
-    if not(root.left) or not(root.right):
+def checkHeap(head):
+    if head == None:
         return True
 
-    left = isHeap(root.left)
-    right = isHeap(root.right)
+    rootData = head.data
+    if head.left and head.right:
+        if rootData < head.left.data or rootData < head.right.data:
+            return False
+    return checkHeap(head.left) and checkHeap(head.right)
 
-    rootData = root.data
 
-    if left == False or right == False:
-        return False
-    elif root.left and root.right and (rootData > root.left.data and rootData > root.right.data):
-        return True
-    elif root.left and not(root.right) and rootData > root.left.data:
-        return True
-    elif root.right and not(root.left) and rootData > root.right.data:
-        return True
-    else:
+def checkHeapHeight(head):
+    if head == None:
+        return 0
+
+    left = checkHeapHeight(head.left)
+    right = checkHeapHeight(head.right)
+    if type(left) == bool or type(right) == bool:
         return False
 
+    if left < right or abs(left-right) > 1:
+        return False
+    return max(left, right)+1
 
-print(isHeap(nodes))
+
+root.print_tree(nodes)
+condition1 = checkHeapHeight(nodes)
+condition2 = checkHeap(nodes)
+
+print(condition1)
