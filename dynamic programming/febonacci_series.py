174c4603aa178de4089dac4a4409b038a9e2f504
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-11-01 09:43:07 +0530

    Modifing [febonacci_series.py] file

--- dynamic programming/febonacci_series.py
+++ dynamic programming/febonacci_series.py
@@ -2,7 +2,7 @@
 # with simple recursion solution
 count = 0
 
-n = 10
+n = 15
 
 
 def feboSimple(n):
@@ -14,7 +14,7 @@ def feboSimple(n):
 
 
 print("\nWith simple solution following is answer ")
-print(f"{n}th febo series is : ", feboSimple(10))
+print(f"{n}th febo series is : ", feboSimple(n))
 print("Number of recursion call is : ", count)
 print()
 print("--"*20)
@@ -39,3 +39,26 @@ arr = [0]*(n+1)
 print("\nWith DP solution following is answer ")
 print(f"{n}th febo series is : ", feboDP(n))
 print("Number of recursion call is : ", count1)
+
+
+# Febo series using varible
+
+def feboTabular(n):
+    if n <= 1:
+        return 1
+
+    first = 1
+    second = 1
+
+    temp = 0
+    for num in range(2, n):
+        temp = first+second
+        first = second
+        second = temp
+
+    return temp
+
+
+print("\nWith DP Tabular format is answer ")
+print(f"{n}th febo series is : ", feboTabular(n))
+print("Number of recursion call is : ", n)
