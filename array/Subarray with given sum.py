a98d07bff1b34c1c1f9518672ef9ad1b0efd3e95
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-11-10 17:08:35 +0530

    subarray with given sum modifing and new method is added

--- array/Subarray with given sum.py
+++ array/Subarray with given sum.py
@@ -6,12 +6,12 @@ arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 def naive_solution(arr, s):
     n = len(arr)
     for i in range(n):
-        local1 = arr[i]
-        for j in range(i+1, n):
+        local1 = 0
+        for j in range(i, n):
             local1 += arr[j]
             if local1 == s:
                 return [i+1, j+1]
-            elif local1 > s:
+            if local1 > s:
                 break
     return [-1]
 
@@ -54,7 +54,58 @@ def subarraySum(arr,  s):
         return -1
 
 
-s = 18
-arr = [1, 2, 3, 7, 5]
-print(naive_solution(arr, s))
-print(subarraySum(arr, s))
+def subarraySum_without_space(arr, k):
+    n = len(arr)
+    for i in range(n):
+        local = 0
+        for j in range(i, n):
+            local += arr[j]
+            if local == k:
+                return [i+1, j+1]
+    return -1, -1
+
+
+def subarraySum_with_space(arr, k):
+    sumi = 0
+    dist = {0: 0}
+    n = len(arr)
+    for i in range(n):
+        sumi += arr[i]
+        diff = sumi-k
+        if diff in dist:
+            return dist[diff], i
+        else:
+            dist[sumi] = i+1
+
+    return -1
+
+
+def subArraySum_without_space(arr, s):
+    begin = 0
+    end = 0
+    n = len(arr)
+    curr_sum = arr[begin]
+
+    while begin != n:
+
+        if curr_sum == s:
+            return begin + 1, end + 1
+
+        elif curr_sum < s and end < n-1:
+            end += 1
+            curr_sum += arr[end]
+
+        elif begin == end and end < n-1:
+            end += 1
+            curr_sum += arr[end]
+
+        else:
+            curr_sum -= arr[begin]
+            begin += 1
+
+    return [-1]
+
+
+s = 13
+arr = [2, 5, 8]
+print(subArraySum_without_space(arr, s))
