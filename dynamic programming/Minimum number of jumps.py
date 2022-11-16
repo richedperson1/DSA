70a19858a285a2faf849194f02a7720ba499bb43
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-11-16 14:49:02 +0530

    Adding [Minimum number of jumps.py] file into the repository

--- dynamic programming/Minimum number of jumps.py
+++ dynamic programming/Minimum number of jumps.py
@@ -9,16 +9,29 @@ import sys
 sys.setrecursionlimit(10**9)
 file = "dynamic programming\\input\\minimum number jump to reach end.txt"
 arr = [1, 4, 3, 2, 6, 7, 8, 1, 2]
-arr = list(map(int, "2 3 1 1 2 4 2 0 1 1".split()))
+arr = list(map(int, "5 9 3 2 1 0 2 3 3 1 0 0".split()))
 
-with open(file) as f:
-    num = []
-    value = f.readlines()
-    num = [int(value[0])]
-    arr = list(map(int, value[1].split()))
+# with open(file) as f:
+#     num = []
+#     value = f.readlines()
+#     num = [int(value[0])]
+#     arr = list(map(int, value[1].split()))
 
 
 n = len(arr)
+
+# 5 9 3 2 1 0 2 3 3 1 0 0
+
+
+def minimum_jump_brute_force(arr, ind, n):
+    if ind >= n:
+        return 0
+
+    ans = sys.maxsize
+    for i in range(ind, ind+arr[ind]):
+        ans = min(ans, minimum_jump_brute_force(arr, i+1, n)+1)
+
+    return ans
 
 
 def minimum_cost(arr, ind, n, dp):
@@ -39,6 +52,28 @@ def minimum_cost(arr, ind, n, dp):
     return ans
 
 
-dp = [-4]*(n+1)
-ans = minimum_cost(arr, 0, n-1, dp)
-print(ans)
+def minimum_jump_optimize(arr):
+    if len(arr) <= 1:
+        return 0
+
+    if arr[0] == 0:
+        return 0
+
+    maxReach = arr[0]
+    step = arr[0]
+    num = len(arr)
+    jump = 1
+    for i in range(1, n):
+        if i == n-1:
+            return jump
+
+        step -= 1
+        if step == 0:
+            if i >= maxReach:
+                return -1
+            step = maxReach-i
+    return jump
+
+
+arr = [1, 3, 5, 7, 9, 3]
+print(minimum_jump_brute_force(arr, 0, len(arr)-1))
