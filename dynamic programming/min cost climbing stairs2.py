843a1052d2a1a777d42dd8f441dd1cbd9ab9bf41
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-12-07 10:49:00 +0530

    Modifing [min cost climbing stairs2.py] file

--- dynamic programming/min cost climbing stairs2.py
+++ dynamic programming/min cost climbing stairs2.py
@@ -39,29 +39,61 @@ def miniCost2(arr, ind, prev):
         return miniCost2(arr, ind+1, True)+arr[ind]
 
 
-def miniCostDP(arr, ind, prev, dp):
-    # if ind == 0:
-    #     return arr[ind]
-    if ind >= len(arr):
+def miniCostDP(arr, ind, dp):
+    if ind < 0:
         return 0
+    if ind == 0 or ind == 1:
+        arr[ind]
 
     if dp[ind] != -1:
         return dp[ind]
 
-    if prev == True:
-        c1 = miniCostDP(arr, ind+1, True, dp)+arr[ind]
-        c2 = miniCostDP(arr, ind+1,  False, dp)
-        dp[ind] = min(c1, c2)
-        return min(c1, c2)
-    else:
-        dp[ind] = miniCostDP(arr, ind+1, True, dp)+arr[ind]
-        return dp[ind]
+    ans = arr[ind]+min(miniCostDP(arr, ind-1, dp), miniCostDP(arr, ind-2, dp))
+    dp[ind] = ans
+    return ans
 
 
-arr = [10, 15, 20]
+"""
+Tabular DP
+"""
+
+
+def minCostTabDP(arr):
+    dp = [arr[0], arr[1]]
+
+    n = len(arr)
+    for i in range(2, n):
+        ans = arr[i]+min(dp[i-1], dp[i-2])
+        dp.append(ans)
+    return min(dp[-1], dp[-2])
+
+
+"""
+Most optimize DP solution
+"""
+
+
+def minCostTabDP2(arr):
+
+    n = len(arr)
+    first = arr[0]
+    second = arr[1]
+    ans = 0
+    for i in range(2, n):
+        ans = arr[i]+min(first, second)
+        first = second
+        second = ans
+    return min(first, second)
+
+
 arr = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+arr = [10, 15, 20]
 
 
 print(miniCost2(arr, 0, True))
-dp = [-1]*(len(arr)+1)
-print(miniCostDP(arr, 0, True, dp))
+n = len(arr)-1
+dp = [-1]*(n+2)
+l1 = min(miniCostDP(arr, n, dp), miniCostDP(arr, n-1, dp))
+print(l1)
+print(minCostTabDP(arr))
+print(minCostTabDP2(arr))
