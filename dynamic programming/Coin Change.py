e36e42c070904f4e5a0bd93585c66f073591fba3
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-12-17 12:35:34 +0530

    adding coin change problem.py

--- dynamic programming/Coin Change.py
+++ dynamic programming/Coin Change.py
@@ -1,14 +1,12 @@
 """
-URL : https://leetcode.com/problems/coin-change/
+maximum coin we can generate using total number
 """
 import sys
-coins = [1, 2, 5]
-amount = 9
 
 
 def cointRequire(coins, total):
     if total < 0:
-        return sys.maxsize
+        return -sys.maxsize
     if total == 0:
         return 0
 
@@ -16,7 +14,26 @@ def cointRequire(coins, total):
     second = cointRequire(coins, total-coins[1])+1
     third = cointRequire(coins, total-coins[2])+1
 
-    return min(first, second, third)
+    return max(first, second, third)
 
+
+def coinChange(coins, total):
+    n = total+1
+    dp = [0]*(n)
+    for i in range(1, n):
+        ans = -sys.maxsize
+        for coin in coins:
+            if i-coin >= 0:
+                local = dp[i-coin]+1
+                ans = max(ans, local)
+
+        dp[i] = ans
+
+    return dp[-1]
+
+
+coins = [6, 2, 5]
+amount = 9
 
 print(cointRequire(coins, amount))
+print(coinChange(coins, amount))
