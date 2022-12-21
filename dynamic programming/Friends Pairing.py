c54cd0bcf8fcc781b6909b8b7dfaff719db787af
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-12-21 15:47:07 +0530

    Adding friends pairing.py

---
+++ dynamic programming/Friends Pairing.py
@@ -0,0 +1,48 @@
+
+def friendPair(n):
+    if n <= 0:
+        return 0
+
+    if n <= 2:
+        return n
+
+    total = friendPair(n-1)+friendPair(n-2)*(n-1)
+
+    return total
+
+
+def friendPairDP(n, dp):
+    if n <= 0:
+        return 0
+
+    if n <= 2:
+        return n
+
+    if dp[n] != -1:
+        return dp[n]
+
+    total = (friendPairDP(n-1, dp) % 1000000007 +
+             (friendPairDP(n-2, dp)*(n-1)) % 1000000007) % 1000000007
+
+    dp[n] = total
+    return total
+
+
+def countFriendsPairings(n):
+    dp = [0]*(n+1)
+    dp[1] = 1
+    dp[2] = 2
+
+    for num in range(3, n+1):
+        first = dp[num-1] % 1000000007
+        second = ((num-1)*dp[num-2]) % 1000000007
+        dp[num] = (first+second) % 1000000007
+
+    return dp[-1]
+
+
+n = 10000
+# print(friendPair(n)).
+dp = [-1]*(n+1)
+# print(friendPairDP(n, dp))
+print(countFriendsPairings(n))
