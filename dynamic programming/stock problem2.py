0fc06191fb1058877e27189046f7ac9c905bb858
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-12-31 18:12:06 +0530

    Adding new method

--- dynamic programming/stock problem2.py
+++ dynamic programming/stock problem2.py
@@ -60,8 +60,51 @@ def maximumProfitDP(ind, buyf, dp):
         return maxi
 
 
+def maximumProfitTabDP(arr):
+    n = len(arr)
+    dp = [[0]*2 for i in range(n+2)]
+    buyf = 1
+    for day in range(n-1, -1, -1):
+        for operation in range(2):
+            if operation:
+                karido = -arr[day]+dp[day+1][0]
+                chodo = dp[day+1][1]
+                maxi = max(karido, chodo)
+                dp[day][operation] = maxi
+            else:
+                becho = arr[day]+dp[day+1][1]
+                chodo = dp[day+1][0]
+                maxi = max(becho, chodo)
+                dp[day][operation] = maxi
+
+    return dp[0][1]
+
+
+def maximumProfitTabSpaceOptDP(arr):
+    n = len(arr)
+    dpNext = [0, 0]
+    for day in range(n-1, -1, -1):
+        dpCurr = [0, 0]
+        for operation in range(2):
+            if operation:
+                karido = -arr[day]+dpNext[0]
+                chodo = dpNext[1]
+                maxi = max(karido, chodo)
+                dpCurr[operation] = maxi
+            else:
+                becho = arr[day]+dpNext[1]
+                chodo = dpNext[0]
+                maxi = max(becho, chodo)
+                dpCurr[operation] = maxi
+
+        dpNext = dpCurr.copy()
+    return dpNext[1]
+
+
 print(maximumProfit(0, 1))
 n = len(arr)
 dp = [[-1]*2 for i in range(n)]
 
 print(maximumProfitDP(0, 1, dp))
+print(maximumProfitTabDP(arr))
+print(maximumProfitTabSpaceOptDP(arr))
