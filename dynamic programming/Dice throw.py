31a3e5f70a0a755c0d610c58f979872bfad15d8f
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-01-12 14:41:40 +0530

    Adding dice through

--- dynamic programming/Dice throw.py
+++ dynamic programming/Dice throw.py
@@ -2,10 +2,6 @@
 URL : https://practice.geeksforgeeks.org/problems/dice-throw5349/1
 """
 
-n = 3
-m = 2
-x = 6
-
 
 def diceThrow(n, m, x):
     if (x < 0) or (n == 0 and x != 0) or (x == 0 and n != 0):
@@ -23,5 +19,47 @@ def diceThrow(n, m, x):
     return final
 
 
+def diceThrowTab(n, m, x):
+    dp = [[0]*(x+1) for i in range(n+1)]
+    dp[0][0] = 1
+
+    for nn in range(1, n+1):
+        for tar in range(1, x+1):
+            final = 0
+            for face in range(1, m+1):
+                if tar-face >= 0:
+                    final += dp[nn-1][tar-face]
+
+            dp[nn][tar] = final
+
+    return dp[n][-1]
+
+
+def diceThrowTabOpt(n, m, x):
+    # dp = [[0]*(x+1) for i in range(n+1)]
+    dp[0][0] = 1
+
+    dpCurr = [0]*(x+1)
+    dpBack = [0]*(x+1)
+    dpBack[0] = 1
+    for nn in range(1, n+1):
+        for tar in range(1, x+1):
+            final = 0
+            for face in range(1, m+1):
+                if tar-face >= 0:
+                    final += dpBack[tar-face]
+
+            dpCurr[tar] = final
+        dpBack = dpCurr.copy()
+
+    return dpCurr[-1]
+
+
+n = 13
+m = 3
+x = 16
+
 dp = [[-1]*(x+1) for i in range(n+1)]
 print(diceThrow(n, m, x))
+print(diceThrowTab(n, m, x))
+print(diceThrowTabOpt(n, m, x))
