b2ee3d9383b4dee595b624d6bd9d5a8b225a29dc
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-01-12 20:20:46 +0530

    Adding new method in the partition equal sum problem

--- dynamic programming/Partition Equal Subset Sum.py
+++ dynamic programming/Partition Equal Subset Sum.py
@@ -1,6 +1,6 @@
 
 
-arr = [1, 5, 11, 5]
+arr = [1, 5, 11, 14]
 
 
 def PartitionSum(ind, arr, t1, t2):
@@ -34,6 +34,39 @@ def PartitionSumDP(ind, arr, t1, t2, dp):
     return ans
 
 
+def partiesSum(ind, arr, x, sumi):
+    if ind >= len(arr):
+        return 0
+
+    if x == sumi:
+        return 1
+
+    inc = partiesSum(ind+1, arr, x, sumi+arr[ind])
+    enc = partiesSum(ind+1, arr, x, sumi)
+
+    return max(inc, enc)
+
+
+def partiesSumDP(ind, arr, x, sumi, dp):
+    if ind >= len(arr):
+        return 0
+
+    if x == sumi:
+        return 1
+
+    if sumi > x:
+        return 0
+
+    if dp[sumi][ind] != -1:
+        return dp[sumi][ind]
+
+    inc = partiesSumDP(ind+1, arr, x, sumi+arr[ind], dp)
+    enc = partiesSumDP(ind+1, arr, x, sumi, dp)
+    ans = max(inc, enc)
+    dp[sumi][ind] = ans
+    return ans
+
+
 print(PartitionSum(0, arr, 0, 0))
 
 sumi = sum(arr)
@@ -43,3 +76,9 @@ dp = [[[-1]*(n+1) for i in range(sumi+1)] for j in range(sumi+1)]
 
 
 print(PartitionSumDP(0, arr, 0, 0, dp))
+
+if sumi & 1 == 0:
+    tar = sumi//2
+    print(partiesSum(0, arr, tar, 0))
+    dp = [[-1]*(len(arr)+1) for j in range(tar+1)]
+    print(partiesSumDP(0, arr, tar, 0, dp))
