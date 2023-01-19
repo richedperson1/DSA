6a1d35a2b38197f9bddb710b24ebbf08d07908fc
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-01-19 10:25:17 +0530

    Adding Guess number higher or lower 2.py

--- array/Maximum Sum Circular Subarray.py
+++ array/Maximum Sum Circular Subarray.py
@@ -30,5 +30,24 @@ def maxiSumSubarray(arr):
     return max(globalMax, final)
 
 
+def maxSubarraySum(arr):
+    currMax = 0
+    globalMax = arr[0]
+    currMin = 0
+    globalMin = arr[0]
+    n = len(arr)
+    for i in range(1, n):
+        currMax = max(currMax+arr[i], arr[i])
+        globalMax = max(globalMax, currMax)
+
+        currMin = min(currMin+arr[i], arr[i])
+        globalMin = min(currMin, globalMin)
+
+    total = sum(arr)
+    if globalMax < 0:
+        return globalMin
+    return max(total-globalMin, globalMax)
+
+
 arr = [1, -2, 3, -2]
 print(maxiSumSubarray(arr))
---
+++ dynamic programming/tree/Binary Tree/Guess Number Higher or Lower II.py
@@ -0,0 +1,57 @@
+
+
+n = 10
+
+
+def guessNumber(i, n):
+    if i >= n:
+        return 0
+
+    ans = float("inf")
+
+    for start in range(i, n):
+        ans = min(ans, start+max(guessNumber(i, start-1),
+                  guessNumber(start+1, n)))
+
+    return ans
+
+
+def guessNumberDP(start, end, dp):
+    if start >= end:
+        return 0
+
+    if dp[start][end] != -1:
+        return dp[start][end]
+
+    ans = float("inf")
+    for i in range(start, end):
+        ans = min(ans, i+max(guessNumberDP(start, i-1, dp),
+                  guessNumberDP(i+1, end, dp)))
+
+    dp[start][end] = ans
+    return ans
+
+
+def guessNumberTabDP(end):
+    n = end
+    dp = [[0]*(n+1)for i in range(n+2)]
+
+    for start in range(n, 0, -1):
+        for end in range(start, n+1):
+            if start == end:
+                continue
+            else:
+                ans = float('inf')
+                for ii in range(start, end+1):
+                    ans = min(ans, ii+max(dp[start][ii-1], dp[ii+1][end]))
+
+                dp[start][end] = ans
+
+    return dp[1][n]
+
+
+print(guessNumber(1, n))
+dp = [[-1]*(n+1)for i in range(n+1)]
+
+print(guessNumberDP(1, n, dp))
+print(guessNumberTabDP(n))
