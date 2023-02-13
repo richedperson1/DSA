765edaf915333416eb2d0478c87d821bc40cb816
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-02-13 17:03:48 +0530

    Adding DFS travels

--- graph/DFS travelsel.py
+++ graph/DFS travelsel.py
@@ -1,10 +1,6 @@
 from typing import *
 from collections import *
-
-
-def adjecent(ans, u, v):
-    ans[u].append(v)
-    ans[v].append(u)
+# from math import *
 
 
 def findAdj(edges, dist):
@@ -12,12 +8,61 @@ def findAdj(edges, dist):
     for ind in range(n):
         u = edges[0][ind]
         v = edges[1][ind]
-        dist = adjecent(dist, u, v)
+        dist[u].append(v)
+        dist[v].append(u)
 
     return dist
 
 
-final = defaultdict(list)
-final["u"].append(5)
-final["u"].append(50)
-print(final)
+def dfs(n, visited, adj, ans):
+    visited[n] = True
+    ans.append(n)
+    for num in adj[n]:
+        if visited.get(num, 0) == 0:
+            dfs(num, visited, adj, ans)
+
+
+def depthFirstSearch(V, E, edges):
+    # write your code here
+    final = defaultdict(list)
+    dist = findAdj(edges, final)
+    final = []
+    visited = {}
+
+    for i in range(V):
+        if visited.get(i, 0) == 0:
+            ans = []
+            dfs(i, visited, dist, ans)
+            final.append(ans)
+    return final
+
+
+string = """
+9 10
+0 8
+1 6
+1 7
+1 8
+5 8
+6 0
+7 3
+7 4
+8 7
+2 5
+"""
+
+edge = [[], []]
+vertix = 4
+for ind, num in enumerate(string.split("\n")):
+
+    if ind == 1:
+        spliting = list(map(int, num.split()))
+        # print(spliting)
+        vertix = spliting[0]
+    if num == "":
+        continue
+    number = list(map(int, num.split()))
+    edge[0].append(number[0])
+    edge[1].append(number[1])
+
+print(depthFirstSearch(vertix, 4, edge))
