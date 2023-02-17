a692c7502ddb6fba3b09216298f8ffcdfe1f2e59
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-02-17 13:53:20 +0530

    Adding minimum distance b/w two node

---
+++ binary tree/Minimum Distance Between BST Nodes.py
@@ -0,0 +1,15 @@
+def minDiffInBST(self, root):
+    self.prev = None
+    self.min_diff = float('inf')
+
+    def inorder(node):
+        if node is None:
+            return
+        inorder(node.left)
+        if self.prev is not None:
+            self.min_diff = min(self.min_diff, node.val - self.prev.val)
+        self.prev = node
+        inorder(node.right)
+
+    inorder(root)
+    return self.min_diff
