a1e54183f7005d356a16cab1cfbf62dbc60493c9
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-02-18 13:15:10 +0530

    adding invert binary tree second methods

--- binary tree/invert binary tree.py
+++ binary tree/invert binary tree.py
@@ -1,10 +1,34 @@
-def invertTree(self, root):
+"""
+URL : https://leetcode.com/problems/invert-binary-tree/
+"""
+from collections import *
+
+
+def invertTree(root):
     if not root:
         return
 
     temp = root.left
     root.left = root.right
     root.right = temp
-    self.invertTree(root.right)
-    self.invertTree(root.left)
+    invertTree(root.right)
+    invertTree(root.left)
     return root
+
+
+def invertTreeQ(root):
+    if not root:
+        return root
+
+    queue = deque([root])
+    temping = root
+    while queue:
+        temp = queue.popleft()
+        temp.left, temp.right = temp.right, temp.left
+        if temp.left:
+            queue.append(temp.left)
+
+        if temp.right:
+            queue.append(temp.right)
+
+    return temping
