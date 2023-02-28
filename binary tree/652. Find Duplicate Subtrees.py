0b207c3e5c89cf3d59a32c4d947acd4d3f6d678d
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-02-28 18:53:52 +0530

    adding find duplicate subtree

---
+++ binary tree/652. Find Duplicate Subtrees.py
@@ -0,0 +1,21 @@
+"""
+URL : https://leetcode.com/problems/find-duplicate-subtrees/description/
+"""
+from collections import *
+visit = defaultdict(str)
+ans = []
+
+
+def duplicateFind(root):
+    if not root:
+        return "Not"
+
+    left = duplicateFind(root.left)
+    right = duplicateFind(root.right)
+    final = str(root.val)+","+left+right
+    if len(visit[final]) == 1:
+        ans.append(root)
+
+    visit[final] = root
+
+    return final
