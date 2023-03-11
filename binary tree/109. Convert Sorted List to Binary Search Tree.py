8b4bb2184af5945ff1a4a07a3072014d28f657eb
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-03-11 20:45:00 +0530

    adding solution for bst from list

---
+++ binary tree/109. Convert Sorted List to Binary Search Tree.py
@@ -0,0 +1,33 @@
+"""
+URL : https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+"""
+
+# Importing library
+from binary_tree import *
+
+
+class treeNode:
+    def __init__(self, data, left=None, right=None) -> None:
+        self.data = data
+        self.right = right
+        self.left = left
+
+
+lst = [1, 2, 3, 4, 5, 6]
+lst = [1, 2, 3, 4, 5, 6, 7]
+
+
+def add_elemet(arr, low, high):
+    if low >= high:
+        return None
+    mid = (low+high)//2
+    newNode = treeNode(arr[mid])
+
+    newNode.left = add_elemet(arr, low, mid)
+    newNode.right = add_elemet(arr, mid+1, high)
+
+    return newNode
+
+
+new_tree = add_elemet(lst, 0, len(lst)-1)
+root.print_tree(new_tree)
