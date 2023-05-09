9bca6f9fb8270330dd753f3f23b7cbade8ba8b45
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-05-09 09:33:18 +0530

    Modifind Kadane's algorithm and added all solution of kadane algorithms and edge case of it.

--- array/Kadane's Algorithm.py
+++ array/Kadane's Algorithm.py
@@ -1,14 +1,91 @@
+"""
+Love Babbar DSA sheet
+
+URL : https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+"""
+
 import sys
 
+"""
+The function finds the maximum sum of a continuous subarray in a given array using a nested loop.
 
-arr = [1, 2, 3, -2, 5]
+:param arr: an input list of integers
+:return: The function `maximumContinuous` returns the maximum sum of a continuous subarray in the
+input array `arr`.
+"""
+ 
+"""
+Time complexity : O(n^2)
+Space complexity : O(1)
+"""
+def maximumContinuous(arr):
+    final = -sys.maxsize
 
-# method 1 brute force approach
-maxi = -sys.maxsize
-# print(maxi)
-curr = 0
-for num in arr:
-    curr = max(num, num+curr)
-    maxi = max(maxi, curr)
+    size = len(arr)
+    for ind in range(size):
+        total = 0
+        for ind2 in range(ind,size):
+            total+=arr[ind2]
+            final = max(final,total)
 
-print(maxi)
+    return final
+
+
+        
+"""
+Time complexity : O(n)
+Space complexity : O(1)
+"""
+
+def maximumContinuous1(arr):
+    final = -sys.maxsize
+
+    size = len(arr)
+    total = final
+    # print(arr)
+    for ind in range(size):
+
+        if total+arr[ind] < arr[ind]:
+            total = arr[ind]
+            final = max(total,final)
+        else:
+            total+=arr[ind]
+            final = max(total,final)
+    return final
+
+"""
+Modified above one
+"""
+
+"""
+The function `maximumContinuous2` returns the maximum sum of a continuous subarray in a given array.
+
+:param arr: The input array of integers for which we need to find the maximum sum of a continuous
+subarray
+:return: The function `maximumContinuous2` is returning the maximum sum of a contiguous subarray in
+the input array `arr`.
+
+"""
+def maximumContinuous2(arr):
+    final = -sys.maxsize
+
+    size = len(arr)
+    total = final
+    # print(arr)
+    for ind in range(size):
+        
+        total = max(total+arr[ind],arr[ind])
+        final = max(total,final)
+        pass
+    return final
+
+
+# Edge case only
+arr = [-5, -2, -6, -8, -1]
+arr = [2, 5, 1, 7, 3]
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+arr = [0]*5
+arr = [5]
+
+print(maximumContinuous1(arr))
+print(maximumContinuous2(arr))
