620892f3e4fb67b8749ad60fda944b360dba7c12
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-07-15 17:00:51 +0530

    adding optimize solution of flood fill

--- graph/flood fill.py
+++ graph/flood fill.py
@@ -1,5 +1,5 @@
+# ! https://practice.geeksforgeeks.org/problems/flood-fill-algorithm1856/1
 """
-! https://practice.geeksforgeeks.org/problems/flood-fill-algorithm1856/1
 Time complexity : O(n*m)
 Space complexity : O(n)
 """
@@ -32,15 +32,45 @@ def floodFill( image, sr, sc, newColor):
     dfs(sr,sc)
     return image
 
+"""
+Time complexity : O(n*m)
+Space complexity : O(1)
+"""
 
+def floodFill2( image, sr, sc, newColor):
+    #Code here
+    mainColor = image[sr][sc]
+    # image[sr][sc] = newColor
+    visit = set()
+    def dfs(row,col):
+        if row>=rows or col>=cols or row<0 or col<0:
+            return 0
+        
+            
+        if image[row][col]==newColor or image[row][col]!=mainColor:
+            return 0
+            
+        local = [[-1,0],[1,0],[0,1],[0,-1]]
+
+        image[row][col] = newColor
+        for r,c in local:
+            newRow = row+r
+            newCol = col+c
+            dfs(newRow,newCol)
+        
+    rows = len(image)
+    cols = len(image[0])
+    
+    dfs(sr,sc)
+    return image
 
 element = '''0 0 1 0 1
 1 1 0 1 0
 0 0 1 1 0
 0 0 1 1 0'''
 
-grid = [[1,1,1],[1,1,0],[1,0,1]]
 grid = [list(map(int,data.split())) for data in element.split('\n')]
+grid = [[1,1,1],[1,1,0],[1,0,1]]
 
 # ( image, sr, sc, newColor)
-print(floodFill(grid,1,3,2))
+print(floodFill2(grid,1,1,2))
