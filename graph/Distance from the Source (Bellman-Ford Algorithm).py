4318c5f4a30d96dd6f152c8a8632b4675620ccb8
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-09-06 10:13:13 +0530

    adding comment for the same

--- graph/Distance from the Source (Bellman-Ford Algorithm).py
+++ graph/Distance from the Source (Bellman-Ford Algorithm).py
@@ -2,6 +2,22 @@
 https://practice.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/1
 """
 
+"""
+The function `bellman_ford` implements the Bellman-Ford algorithm to find the shortest path from a
+source vertex to all other vertices in a weighted directed graph.
+
+param v: The parameter `v` represents the number of vertices in the graph
+
+param edges: The "edges" parameter is a list of tuples representing the edges in the graph. Each
+tuple contains three elements: the source vertex, the destination vertex, and the weight of the edge
+
+param s: The parameter "s" represents the source vertex from which the shortest paths are
+calculated
+
+return: The function `bellman_ford` returns a list of distances from the source vertex `s` to all
+other vertices in the graph.
+"""
+
 def bellman_ford( v, edges, s):
     #code here
 
