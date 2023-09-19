047b236ebda18ef91ae47c28c83296946436991a
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-09-19 10:33:53 +0530

    all test case of account marger is pass

--- graph/account marger.py
+++ graph/account marger.py
@@ -1,45 +1,71 @@
 from collections import defaultdict
-
-def accountsMerge( accounts):
+class disJoint:
+    def __init__(self,v):
+        self.parent = [ind for ind in range(v)]
+        self.rank   = [0]*v
     
-    num = len(accounts)
-    parent = [ind for ind in range(num)]
-    dist = defaultdict(bool)
-    for index in range(num):
-        lastPoint = -1
-        size = len(accounts[index])
-        for ind in range(1,size):
-            newEmail = accounts[index][ind]
-            if newEmail not in dist:
-                dist[newEmail] = index
-            else:
-                lastPoint = ind 
-                break
-        if lastPoint != -1:
-            parent[index] = dist[accounts[index][lastPoint]]
+    def findParent(self,u):
+        if self.parent[u]==u:
+            return u
             
-            for ind in range(1,size):
-                dist[accounts[index][ind]] = parent[index]
-
+        self.parent[u] = self.findParent(self.parent[u])
+        return self.parent[u]
+        
+    def unionByRank(self,u,v):
+        ultU = self.findParent(u)
+        ultV = self.findParent(v)
+        
+        if ultU==ultV:
+            return
+        
+        if self.rank[ultV]>self.rank[ultU]:
+            self.parent[ultU] = ultV
+            
+        elif self.rank[ultU]>self.rank[ultV]:
+            self.parent[ultV] = ultU
+            
+        else:
+            self.parent[ultU] = ultV
+            self.rank[ultV] +=1
+            
     
-    mergeMail = defaultdict(list)
-    for data,ind in dist.items():
-        mergeMail[ind].append(data)
-
-
-    newAccount = []
-    for ind,data in mergeMail.items():
-        firstIndex = accounts[ind][0]
-        newData = data.append(firstIndex)
-        newData = sorted(data)
-        newAccount.append(newData)
-
-    return newAccount
-
-    return dist
+class Solution:
+    def accountsMerge(self, accounts):
+        # Code here
+        size = len(accounts)
+        
+        mail2Node = defaultdict(int)
+        dis = disJoint(size)
+        for index in range(size):
+            for pos in range(1,len(accounts[index])):
+                mail = accounts[index][pos]
+                if mail not in mail2Node:
+                    mail2Node[mail] = index
+                else:
+                    dis.unionByRank(index,mail2Node[mail])
+                    
+        
+        finalDist = defaultdict(list)
+        # return mail2Node,dis.parent
+        for data,ind in mail2Node.items():
+            ultParent = dis.findParent(ind)
+            finalDist[ultParent].append(data)
+            
+        
+        finalOut = []
+        for ind,data in finalDist.items():
+            if data==[]:
+                continue
+            
+            sData = sorted(data)
+            sData.insert(0,accounts[ind][0])
+            finalOut.append(sData)
+        return finalOut
+            
 accounts  =[["John","johnsmith@mail.com","john_newyork@mail.com"],
 ["John","johnsmith@mail.com","john00@mail.com"],
 ["Mary","mary@mail.com"],
 ["John","johnnybravo@mail.com"]]
 
-print(accountsMerge(accounts))
+sol = Solution()
+print(sol.accountsMerge(accounts))
