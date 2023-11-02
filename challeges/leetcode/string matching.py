bcc4958b9f977cae4d5704414a57c1de807f661a
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-11-02 21:03:57 +0530

    adding string matching

--- challeges/leetcode/string matching.py
+++ challeges/leetcode/string matching.py
@@ -27,15 +27,12 @@ class Solution:
         distC2W = {}
         if len(s)!=len(pattern):
             return False
-
-
         for char , word in zip(s,pattern):
 
             if distW2C.get(char,-1)==-1 and distC2W.get(word,-1)==-1:
                 distW2C[char] = word
                 distC2W[word] = char
                 continue
-
             if distW2C.get(char,-1)!=word or distC2W.get(word,-1)!=char:
                 return False
 
