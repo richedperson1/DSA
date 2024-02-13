b3f21357054ac982b24e8dde789a87b68dee795e
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2024-02-13 21:38:00 +0530

    adding 235 leetcode quection

---
+++ binary tree/235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -0,0 +1,32 @@
+from binary_tree import *
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q) :
+        ans = set([])
+        ans1 = set([])
+        def dfs(root,tar,ans):
+            if not root:
+                return
+
+            left = dfs(root.left,tar,ans)
+            right= dfs(root.right,tar,ans)
+            if left or right:
+                ans.add(root)
+                return True
+            co
+            if root.val==tar:
+                return True
+            return False
+        dfs(root,p,ans)
+        dfs(root,q,ans1)
+        z = ans.intersection(ans1)
+        print(ans,ans1,z)
+        return root
+    
+null = -1
+arr = [6,2,8,0,4,7,9,null,null,3,5]
+p = 2
+q = 4
+tree = createTree().createTreeUsingList(arr)
+obj = Solution()
+obj.lowestCommonAncestor(tree,p,q)
