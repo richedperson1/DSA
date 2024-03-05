8fe1d8459494b0004305ef13224a8f380b0b40df
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2024-03-05 10:58:22 +0530

    modifing 1372 problem of leetcode

--- binary tree/1372. Longest ZigZag Path in a Binary Tree.py
+++ binary tree/1372. Longest ZigZag Path in a Binary Tree.py
@@ -4,6 +4,17 @@ URL : https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/
 
 from binary_tree import *
 
+"""
+The function `longZigZap` recursively calculates the length of the longest zigzag path in a binary
+tree.
+
+:param root: The `root` parameter in the `longZigZap` function is the root node of a binary tree.
+The function `longZigZap` is trying to find the length of the longest zigzag path in the binary tree
+:return: The function `longZigZap` is returning the result of calling the helper function with the
+root node of a tree, an initial previous direction of -1, and a total count of 0. The helper
+function recursively traverses the tree in a zigzag pattern and calculates the maximum length of the
+zigzag path. The final result of the helper function is returned by the `longZ
+"""
 
 def longZigZap(root):
     if not root:
@@ -30,6 +41,18 @@ def longZigZap(root):
 
     return helper(root, -1, 0)
 
+
+def longZipZag2(root):
+    ans = 0
+    def dfs(root):
+        nonlocal ans
+        if not root:
+            return -1,-1
+        
+        left  = longZipZag2(root.left)[1]+1
+        right = longZipZag2(root.right)[0]+1
+        ans   = max(left,right,ans)
+        return left,right
 
 ListNode = [1, -1, 1, 1, 1, -1, -1, 1,
             1, -1, 1, -1, -1, -1, 1, -1, 1]
