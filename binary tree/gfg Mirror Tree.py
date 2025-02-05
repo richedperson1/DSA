199a71d03c854a94a6ee20c67f5cb6801b682d47
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2025-02-05 23:19:40 +0530

    adding gfg mirror tree

---
+++ binary tree/gfg Mirror Tree.py
@@ -0,0 +1,35 @@
+# The `Solution` class contains two methods, `mirror1` and `mirror2`, which are used to convert a
+# binary tree into its mirror tree using depth-first search.
+
+class Solution:
+    #Function to convert a binary tree into its mirror tree.
+    def mirror1(self, root):
+        # Code here
+        def dfs(root):
+            if not root:
+                return 
+            
+            root.left,root.right = root.right,root.left
+            left = dfs(root.left)
+            right= dfs(root.right)
+            
+            # root.right = left
+            # root.left = right
+            # return root
+            
+        dfs(root)
+        return root
+    def mirror2(self, root):
+        # Code here
+        def dfs(root):
+            if not root:
+                return 
+            
+            # root.left,root.right = root.right,root.left
+            left = dfs(root.left)
+            right= dfs(root.right)
+            
+            root.right = left
+            root.left = right
+            return root
+        return dfs(root)
